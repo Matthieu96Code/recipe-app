15c28fad4285d13ee859393297dce33583cd5257
Issah Mudasir <danladi496@gmail.com>
2023-06-29 09:22:38 +0000

    Controllers-models-added

--- Gemfile
+++ Gemfile
@@ -2,6 +2,19 @@ source "https://rubygems.org"
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.2.2"
+gem 'bootstrap_form'
+gem 'cancancan'
+gem 'devise'
+gem 'factory_bot_rails'
+gem 'jquery-rails'
+gem 'letter_opener'
+gem 'rack-cors'
+gem 'rails-controller-testing'
+gem 'rspec-rails', '~> 5.0.0'
+gem 'rswag'
+gem 'rubocop', '>= 1.0', '< 2.0'
+gem 'rubocop-discourse'
+gem 'shoulda-matchers', '~> 4.0'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.5"
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
+  
+  
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login', sign_out: 'logout',
+    password: 'secret', confirmation: 'verification',
+    registration: 'register', edit: 'edit/profile'
+  }, sign_out_via: [:get, :post]
+
+  resources :recipes, only: [:index, :show, :new, :edit, :create, :destroy]
+  resources :users
+  resources :recipe_foods
+  resources :foods, only: [:index, :show, :new, :edit,:create, :destroy]
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "foods#index"
 end
