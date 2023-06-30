2477900f1a133662c24035e3567712e7b5a56750
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-06-30 11:12:39 +0000

    Remove unused active record

--- db/migrate/20230630105131_add_quantity_and_recipe_ref_to_recipe_foods.rb
+++
@@ -1,6 +0,0 @@
-class AddQuantityAndRecipeRefToRecipeFoods < ActiveRecord::Migration[7.0]
-  def change
-    add_column :recipe_foods, :quantity, :integer
-    add_reference :recipe_foods, :recipe, null: false, foreign_key: true
-  end
-end
