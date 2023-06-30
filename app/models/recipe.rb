6e26ac99407a3f7dedeef1b1222f3e50abaa1d70
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-06-30 14:48:22 +0000

    Update recipe application recored with association and toggle method

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,18 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  has_many :recipe_foods, dependent: :destroy
+  validates :name, presence: true 
+  validates :preparation_time, numericality: { greater_than_or_equal_to: 0 }
+  validates :cooking_time, numericality: { greater_than_or_equal_to: 0 }
+  validates :user_id, presence: true
+
+  scope :public_recipes, -> { where(public: true).order(created_at: :desc) }
+
+  def toggle_privacy!
+    update(public: !public)
+  end
+
+  def toggle_shopping_tag!
+    update(shopping_tag: !shopping_tag)
+  end
 end
